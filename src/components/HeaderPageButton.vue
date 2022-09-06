e78005032eb39c9eefbf01c449b9f4ba0bc6c72e
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-05 22:39:49 -0300

    create header page button

---
+++ src/components/HeaderPageButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="pageButton" :class="{ selected }">
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "HeaderPageButton",
+  props: {
+    label: "",
+    path: "",
+    selected: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.pageButton {
+  min-width: 274px;
+  height: 42px;
+  border-radius: 21px;
+  border: 0px;
+  background-color: transparent;
+  color: var(--white);
+  letter-spacing: 0.64px;
+  font-size: 16px;
+  font-family: var(--star-jedi);
+  transition: 0.2s;
+}
+
+.selected, .pageButton:hover {
+  background-color: var(--light-gray);
+}
+</style>
