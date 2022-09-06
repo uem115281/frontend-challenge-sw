519480e1f1698946ec8de8909efa3907fc040f21
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-06 20:19:27 -0300

    use text input component

--- src/pages/ListaPersonagens.vue
+++ src/pages/ListaPersonagens.vue
@@ -1,31 +1,43 @@
 <template>
   <div class="container">
     <h1>Personagens</h1>
+    <TextInput
+      placeholder="FILTRE POR NOME DO PERSONAGEM"
+      class="inputCadastro"
+    />
   </div>
 </template>
-  
-  <script>
+
+<script>
+import TextInput from "@/components/TextInput.vue";
+
 export default {
   name: "ListaPersonagens",
   props: {},
+  components: {
+    TextInput,
+  },
 };
 </script>
-  
-<style scoped>
-  .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: start;
-  }
 
-  .container h1 {
-    font-family: var(--star-jedi);
-    color: var(--yellow);
-    height: 64px;
-    font-size: 40px;
-    letter-spacing: 1.6px;
-    margin-top: 48px;
-  }
+<style scoped>
+.inputCadastro {
+  margin-top: 50px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+}
+
+.container h1 {
+  font-family: var(--star-jedi);
+  color: var(--yellow);
+  height: 64px;
+  font-size: 40px;
+  letter-spacing: 1.6px;
+  margin-top: 48px;
+}
 </style>
-  
