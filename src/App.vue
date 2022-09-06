a183ce22d08b298933f64370b9e394f60cce7724
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-05 22:28:17 -0300

    use header component, set colors and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <HeaderComponent></HeaderComponent>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComponent,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0px;
+  margin: 0px;
+}
+
+@font-face {
+  font-family: "Star Jedi Regular";
+  src: url("./assets/fonts/Starjedi.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Roboto Medium";
+  src: url("./assets/fonts/Roboto-Medium.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Roboto Regular";
+  src: url("./assets/fonts/Roboto-Regular.ttf") format("truetype");
+}
+
+:root {
+  --yellow: #ffe81f;
+  --light-gray: #303030;
+  --white: #fafafa;
+  --dark: #0f0f0f;
+  --star-jedi: "Star Jedi Regular";
+  --roboto-medium: "Roboto Medium";
+  --roboto-regular: "Roboto Regular";
+}
+
+.container {
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #000;
 }
 </style>
