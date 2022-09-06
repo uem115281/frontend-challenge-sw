4628bc8993abcfc02a3eb6020ef72797724091be
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-05 22:46:25 -0300

    create my button component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="buttonContainer">
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  props: {
+    label: "",
+  },
+};
+</script>
+
+<style scoped>
+.buttonContainer {
+  height: 42px;
+  padding-left: 17px;
+  padding-right: 17px;
+  border: 2px solid var(--yellow);
+  font-size: 15px;
+  color: var(--white);
+  background-color: transparent;
+  font-family: var(--star-jedi);
+  cursor: pointer;
+}
+
+.buttonContainer:hover {
+  background-color: var(--light-gray);
+}
+</style>
