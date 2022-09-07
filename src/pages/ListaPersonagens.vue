5c4bce8f79ab48a872695382a4d476feccda880d
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-06 21:15:10 -0300

    implement PersonagemCard

--- src/pages/ListaPersonagens.vue
+++ src/pages/ListaPersonagens.vue
@@ -5,22 +5,40 @@
       placeholder="FILTRE POR NOME DO PERSONAGEM"
       class="inputCadastro"
     />
+    <div class="personagensList">
+      <PersonagemCard v-for="personagem in personagens" :key="personagem.url" :personagem="personagem" />
+    </div>
   </div>
 </template>
 
 <script>
 import TextInput from "@/components/TextInput.vue";
+import PersonagemCard from "@/components/PersonagemCard.vue";
 
 export default {
   name: "ListaPersonagens",
   props: {},
   components: {
     TextInput,
-  },
+    PersonagemCard
+},
+  computed: {
+    personagens() {
+      return new Array(20).fill({});
+    }
+  }
 };
 </script>
 
 <style scoped>
+.personagensList {
+  display: grid;
+  grid-template-columns: 273px 273px 273px 273px;
+  gap: 24px;
+  column-gap: 24px;
+  margin-top: 32px;
+}
+
 .inputCadastro {
   margin-top: 50px;
 }
