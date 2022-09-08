96627869ac2048a6c6029c4d32e0fc9401fa37a6
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-07 23:11:06 -0300

    finish cadastro casting page

--- src/pages/CadastroCasting.vue
+++ src/pages/CadastroCasting.vue
@@ -3,7 +3,7 @@
     <h1>Participe do próximo filme</h1>
     <form action="#" class="formularioCadastro">
       <h2>Digite suas informações</h2>
-      <div class="subDivForm">
+      <div class="firstSubDivForm">
         <h3>informações Gerais</h3>
         <div class="column">
           <div class="textField">
@@ -54,19 +54,43 @@
           </div>
         </div>
       </div>
-      <div class="subDivForm">
+      <div class="secondSubDivForm">
         <h3>Sobre o seu contato com Star Wars</h3>
+        <div class="selectField">
+          <label>QUAL SEU PERSONAGEM FAVORITO?</label>
+          <MySelect
+            :data="personagens"
+            placeholder="Selecione seu personagem"
+            class="selectPersonagem"
+          />
+        </div>
+        <div class="textField">
+          <label>UM RESUMO DA SUA CARREIRA ARTISTICA</label>
+          <MyTextarea
+            class="textResumo"
+            placeholder="Escreva aqui o resumo"
+          />
+        </div>
+        <div class="fileField">
+          <label for="curriculo">ANEXE SEU CURRÍCULO</label>
+          <FileInput @changeFile="handleChange"></FileInput>
+        </div>
       </div>
+      <MyButton label="Enviar" class="buttonSend" />
     </form>
   </div>
 </template>
   
 <script>
 import TextInput from "@/components/TextInput.vue";
+import MySelect from "@/components/MySelect.vue";
+import FileInput from "@/components/FileInput.vue";
+import MyTextarea from "@/components/MyTextarea.vue";
+import MyButton from "@/components/MyButton.vue";
 export default {
   name: "CadastroCasting",
   props: {},
-  components: { TextInput },
+  components: { TextInput, MySelect, FileInput, MyTextarea, MyButton },
   data: function () {
     return {
       nome: "",
@@ -75,17 +99,47 @@ export default {
       telefone: "",
       senha: "",
       confirmarSenha: "",
+      personagens: [{
+        value: 1,
+        label: 'personagem 1'
+      }, {
+        value: 2,
+        label: 'personagem 2'
+      }, {
+        value: 3,
+        label: 'personagem 3'
+      }, {
+        value: 4,
+        label: 'personagem 4'
+      }]
     };
   },
+  methods: {
+    handleChange(evt) {
+      console.log(evt);
+    }
+  }
 };
 </script>
   
 <style scoped>
+.buttonSend {
+  align-self: center;
+  margin-top: 24px;
+  color: var(--yellow);
+  height: 42px;
+  width: 372px;
+}
+
+.selectPersonagem {
+  width: 257px;
+}
+
 .textField input {
   width: 100%;
 }
 
-.textField label {
+.textField label, .selectField label, .fileField label {
   display: flex;
   align-items: center;
   font-size: 14px;
@@ -105,7 +159,11 @@ export default {
   gap: 24px;
 }
 
-.subDivForm h3 {
+.secondSubDivForm h3 {
+  margin-bottom: -8px;
+}
+
+.firstSubDivForm h3, .secondSubDivForm h3 {
   grid-column: 1 / 3;
   font-family: var(--star-jedi);
   color: var(--white);
@@ -114,7 +172,15 @@ export default {
   font-weight: 100;
 }
 
-.subDivForm {
+.secondSubDivForm {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-top: 24px;
+  width: 100%;
+}
+
+.firstSubDivForm {
   display: grid;
   grid-template-columns: 257px 257px;
   gap: 24px;
@@ -157,6 +223,7 @@ export default {
   font-size: 40px;
   letter-spacing: 1.6px;
   margin-top: 48px;
+  margin-bottom: 40px;
 }
 </style>
   
